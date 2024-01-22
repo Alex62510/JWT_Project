f72d40ed5fd769244945f99c1b30ec042f1dc779
Alex62510 <eagle625@tut.by>
2024-01-22 17:00:36 +0300

    02-commit add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Home} from "./pages/Home.tsx";
+import {About} from "./pages/About.tsx";
+import {SingIn} from "./pages/SingIn.tsx";
+import {SingUp} from "./pages/SingUp.tsx";
+import {Profile} from "./pages/Profile.tsx";
 
 function App() {
 
-
     return (
-        <div className={'text-amber-600'}>
-            <h1>Hello</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path={'/'} element={<Home/>}/>
+                <Route path={'/about'} element={<About/>}/>
+                <Route path={'/sing-in'} element={<SingIn/>}/>
+                <Route path={'/sing-up'} element={<SingUp/>}/>
+                <Route path={'/profile'} element={<Profile/>}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
